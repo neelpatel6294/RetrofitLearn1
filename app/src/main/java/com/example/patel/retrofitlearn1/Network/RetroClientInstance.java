c02b649c4aba2bca5d736a74d1b4fb837020e8ec
Neel Patel <neelpatel6294@gmail.com>
2018-07-18 11:22:03 -0400

    Second Commit

--- app/src/main/java/com/example/patel/retrofitlearn1/Network/RetroClientInstance.java
+++ app/src/main/java/com/example/patel/retrofitlearn1/Network/RetroClientInstance.java
@@ -1,8 +1,21 @@
 package com.example.patel.retrofitlearn1.Network;
 
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetroClientInstance {
 
     private static Retrofit retrofit;
+
+    private static final String BASE_URL = "https://jsonplaceholder.typicode.com";
+
+    public static Retrofit getRetrofitInstance(){
+        if (retrofit == null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
 }
